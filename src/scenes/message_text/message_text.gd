030e9c6a9912f30d86fbe83366ddc40492c39a6e
Michael Hüneburg <hello@michaelhue.com>
2023-02-11 18:39:37 +0100

    Add translation links for message text
    Adds a "Translate" submenu to the right-click menu of the message text
    field. The submenu contains links to various online translation services
    and will automatically transmit the current locale and text or text
    selection.
    
    Adds links for DeepL, Google and Bing.

--- src/scenes/message_text/message_text.gd
+++ src/scenes/message_text/message_text.gd
@@ -1,6 +1,17 @@
 extends TextEdit
 
 
+const MENU_TRANSLATE = 1000
+const MENU_TRANSLATE_GOOGLE = 1001
+const MENU_TRANSLATE_DEEPL = 1002
+const MENU_TRANSLATE_BING = 1003
+
+const TRANSLATION_SERVICES := {
+	MENU_TRANSLATE_GOOGLE: "https://translate.google.com/?sl={locale}&text={text}&op=translate",
+	MENU_TRANSLATE_DEEPL: "https://www.deepl.com/translator#{locale}/-/{text}",
+	MENU_TRANSLATE_BING: "https://www.bing.com/translator/?from={locale}&text={text}",
+}
+
 ## Name of the locale to use for the font override.
 export var locale: String setget set_locale, get_locale
 
@@ -13,6 +24,9 @@ var _locale_variants := PoolStringArray()
 
 func _ready() -> void:
 	add_color_region("{", "}", Color("#d1fae5"))
+
+	get_menu().add_separator()
+	get_menu().add_submenu_item("Translate", "../TranslateMenu", MENU_TRANSLATE)
 
 
 func set_locale(new_locale: String) -> void:
@@ -42,3 +56,19 @@ func _update_font() -> void:
 		add_font_override("font", font)
 	elif has_font_override("font"):
 		remove_font_override("font")
+
+
+func _on_translate_menu_id_pressed(id: int) -> void:
+	if not TRANSLATION_SERVICES.has(id):
+		return
+
+	var selection := get_selection_text()
+	var stext := selection if not selection.empty() else text
+
+	var url := TRANSLATION_SERVICES[id] as String
+	var params := {
+		"locale": AppState.locale.substr(0, 2),
+		"text": stext.percent_encode(),
+	}
+
+	OS.shell_open(url.format(params))
